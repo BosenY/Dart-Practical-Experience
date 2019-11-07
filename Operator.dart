9cae12673d4f90b32e3cabdf9cdfcd9a027a7bf9
boseny <yxy43445@gmail.com>
2019-11-08 03:08:40 +0800

    add demo

---
+++ Operator.dart
@@ -0,0 +1,29 @@
+void main(List<String> args) {
+  assert(2 + 3 == 5);
+  assert(2 - 3 == -1);
+  assert(2 * 3 == 6);
+  assert(5 / 2 == 2.5); // Result is a double
+  assert(5 ~/ 2 == 2); // Result is an int 会返回一个整数类型
+  assert(5 % 2 == 1); // Remainder
+
+  assert('5/2 = ${5 ~/ 2} r ${5 % 2}' == '5/2 = 2 r 1');
+  print('执行完毕');
+
+  var a, b;
+
+  a = 0;
+  b = ++a; // Increment a before b gets its value.
+  assert(a == b); // 1 == 1
+
+  a = 0;
+  b = a++; // Increment a AFTER b gets its value.
+  assert(a != b); // 1 != 0
+
+  a = 0;
+  b = --a; // Decrement a before b gets its value.
+  assert(a == b); // -1 == -1
+
+  a = 0;
+  b = a--; // Decrement a AFTER b gets its value.
+  assert(a != b); // -1 != 0
+}
--- Variable.dart
+++ Variable.dart
@@ -13,6 +13,7 @@ void noDefaultValue() {
 /**
  * 如果变量的值是一个不变的值，请使用 final 或 const 代替 var 或类型。final 只能赋值一次；const 则是编译时的常量；const 一经声明就不能再次赋值；
  * final 变量必须被初始化 而且只可以被设置一次值，而且！final变量与类变量在首次访问时才执行初始化
+ * const 关键字不只是用于声明常量，还可以用它来创建常量值；以及声明创建常量值的构造函数。任何变量都可以具有常量值。
  */
 void main(List<String> args) {
   noDefaultValue();
@@ -23,6 +24,10 @@ void main(List<String> args) {
   final me = getName(); //只有在首次访问这个变量me的时候才会去初始化
 
   print(me);
+  var foo = const [];
+  foo = [1, 2, 3]; // 第一次赋值；
+  foo = [2, 3, 4222]; // 错误： const变量不能再次赋值；
+  print(foo);
 }
 
 String getName() {
