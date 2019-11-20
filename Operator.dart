80c9aaaae8959734a1841a42934f15611644adfb
boseny <yxy43445@gmail.com>
2019-11-20 21:12:43 +0800

    feat: upgrade

--- Operator.dart
+++ Operator.dart
@@ -4,8 +4,12 @@ void main(List<String> args) {
   assert(2 * 3 == 6);
   assert(5 / 2 == 2.5); // Result is a double
   assert(5 ~/ 2 == 2); // Result is an int 会返回一个整数类型
-  assert(5 % 2 == 1); // Remainder
+  assert(5 % 2 == 2, 21323123); // Remainder
 
+  // 使用 dart --enable-asserts xxx 可以开启assert
+var urlString;
+assert(urlString.startsWith('https'),
+    'URL ($urlString) should start with "https".');
   assert('5/2 = ${5 ~/ 2} r ${5 % 2}' == '5/2 = 2 r 1');
   print('执行完毕');
 
@@ -26,4 +30,13 @@ void main(List<String> args) {
   a = 0;
   b = a--; // Decrement a AFTER b gets its value.
   assert(a != b); // -1 != 0
+  Visibility(false);
+}
+
+
+void Visibility(bool isPublic) {
+  var visibility = isPublic ? 'public' : 'private';
+  var ss = null;
+  print(ss ?? 'ss is null');
+  print(visibility);
 }
---
+++ ProcessControl.dart
@@ -0,0 +1,9 @@
+void main() {
+  for (int i = 0; i < 12; i++) {
+    print(i);
+  }
+  var arr = [1,2,3,4];
+  for(var item in arr) {
+    print(item);
+  }
+}
